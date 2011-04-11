8274fc7acbb727167a87f60b3fb1c3878f9bc54e
Felix Geisendörfer <felix@debuggable.com>
2011-04-12 00:11:45 +0200

    Add HttpLiveStreamingWriter interface and test

--- test/integration/test-http-live-streaming-writer.js
+++ test/integration/test-http-live-streaming-writer.js
@@ -3,10 +3,41 @@ var assert = common.assert;
 var fixture = common.helper.fixture;
 
 var writer = common.m3u.httpLiveStreamingWriter();
-writer.comment('I am a comment');
-writer.info('10', 'Artist - Title');
-writer.info('42');
-writer.uri('foo.mp3');
-writer.uri('bar.mp3', 23);
+// EXT-X-TARGETDURATION: Maximum media file duration.
+writer.targetDuration(10);
+
+// EXT-X-MEDIA-SEQUENCE: Sequence number of first file (optional).
+// (optional)
+writer.mediaSequence(0)
+
+// EXT-X-PROGRAM-DATE-TIME: The date of the program's origin, optional.
+// (optional)
+writer.programDateTime('2011-04-11T21:24:06Z');
+
+// EXT-X-ALLOW-CACHE: Set if the client is allowed to cache this m3u file.
+// (optional)
+writer.allowCache(true);
+writer.allowCache(false);
+
+// EXT-X-PLAYLIST-TYPE: Provides mutability information about the m3u file.
+// (optional)
+writer.playlistType('EVENT');
+writer.playlistType('VOD');
+
+// EXT-X-ENDLIST: Indicates that no more media files will be added to the m3u file.
+// (optional)
+writer.endlist();
+
+// EXT-X-VERSION: Indicates the compatibility version of the Playlist file.
+// (optional)
+writer.version(3);
+
+// Adds a playlist as the next item preceeded by an EXT-X-STREAM-INF tag.
+writer.playlist('another.m3u', {
+  bandwidth: 512000, // required
+  programId: '1',
+  codecs: 'avc1.42001e,mp4a.40.34',
+  resolution: '640x480',
+});
 
 fixture.compare(writer, 'http_live_streaming_writer.m3u8');
